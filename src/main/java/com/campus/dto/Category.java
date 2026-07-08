ce05979e93296193afee6f08630667ec932970c7
amrutha105 <ammuamrutha8166@gmail.com>
2026-07-08 13:02:59 +0530

    category dto added

---
+++ src/main/java/com/campus/dto/Category.java
@@ -0,0 +1,19 @@
+package com.campus.dto;
+
+public class Category {
+	private Integer category_id;
+	private String cname;
+	public Integer getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(Integer category_id) {
+		this.category_id = category_id;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+}
