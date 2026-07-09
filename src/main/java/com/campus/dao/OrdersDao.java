9c3e0204a255037537d8faaf155e691e81c0ade4
maheshbabun2364 <maheshbabun2364@gmail.com>
2026-07-09 13:46:50 +0530

    Orders Dao added

--- src/main/java/com/campus/dao/OrdersDao.java
+++ src/main/java/com/campus/dao/OrdersDao.java
@@ -1,5 +1,16 @@
 package com.campus.dao;
 
+import java.util.List;
+import com.campus.dto.Orders;
+
 public interface OrdersDao {
-	
+	void addOrders(Orders o);
+
+    void updateOrders(Orders o);
+
+    void deleteOrders(int orderId);
+
+    Orders getOrdersById(int orderId);
+
+    List<Orders> getAllOrderItems();
 }
