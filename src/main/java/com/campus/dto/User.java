e43d6e05075cae36ef1383a1c0bdbb07be8ad6d1
Chandana-Ramu <chandanaramu35@gmail.com>
2026-07-09 22:33:03 +0530

    user dto added

--- src/main/java/com/campus/dto/User.java
+++ src/main/java/com/campus/dto/User.java
@@ -1,5 +1,55 @@
 package com.campus.dto;
 
 public class User {
-
+	
+	private Integer user_id;
+	private String name;
+	private String mail;
+	private String password;
+	private String createdAt;
+	private String role;
+	
+	public Integer getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", name=" + name + ", mail=" + mail + ", password=" + password
+				+ ", createdAt=" + createdAt + ", role=" + role + "]";
+	}
+	
 }
