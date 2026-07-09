870a4114fe30d0a32420fbca4c96b12bd4da57e6
maheshbabun2364 <maheshbabun2364@gmail.com>
2026-07-09 13:35:34 +0530

    ordersDto file added

---
+++ src/main/java/com/campus/dao/OrdersDao.java
@@ -0,0 +1,5 @@
+package com.campus.dao;
+
+public interface OrdersDao {
+	
+}
---
+++ src/main/java/com/campus/dto/Orders.java
@@ -0,0 +1,37 @@
+package com.campus.dto;
+
+public class Orders {
+	 private int order_id ;
+	private double total_amount; 
+	private String order_status;  
+	private String ordered_at;
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public double getTotal_amount() {
+		return total_amount;
+	}
+	public void setTotal_amount(double total_amount) {
+		this.total_amount = total_amount;
+	}
+	public String getOrder_status() {
+		return order_status;
+	}
+	public void setOrder_status(String order_status) {
+		this.order_status = order_status;
+	}
+	public String getOrdered_at() {
+		return ordered_at;
+	}
+	public void setOrdered_at(String ordered_at) {
+		this.ordered_at = ordered_at;
+	}
+	@Override
+	public String toString() {
+		return "Orders [order_id=" + order_id + ", total_amount=" + total_amount + ", order_status=" + order_status
+				+ ", ordered_at=" + ordered_at + "]";
+	} 
+}
