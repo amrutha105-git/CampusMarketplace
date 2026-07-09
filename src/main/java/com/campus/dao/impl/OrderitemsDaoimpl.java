346904064afde9e0b9161580912c8369cf7ebb21
meghana554 <meghanam0369@gmail.com>
2026-07-09 21:36:45 +0530

    orderitems daoimpl added

--- src/main/java/com/campus/dao/impl/OrderitemsDaoimpl.java
+++ src/main/java/com/campus/dao/impl/OrderitemsDaoimpl.java
@@ -2,7 +2,9 @@ package com.campus.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.campus.dao.OrderitemsDao;
@@ -38,32 +40,87 @@ public class OrderitemsDaoimpl implements OrderitemsDao {
 
 	@Override
 	public void updateOrderItem(Orderitems o) {
-//		order_item_id int AI PK 
-//		order_id int 
-//		product_id int 
-//		order_quantity int 
-//		unit_price decimal(10,2)
+
 		String query="update order_item set order_id=?,set product_id=?,set order_quantity=?,set unit_price=? where order_item_id=? ";
-		
+		 try {
+	          PreparedStatement ps = con.prepareStatement(query);
+	            ps.setInt(1, o.getOrderId());
+	            ps.setInt(2, o.getProductId());
+	            ps.setInt(3, o.getOrderQuantity());
+	            ps.setDouble(4, o.getUnitPrice());
+	            ps.setInt(5, o.getOrderItemId());
+	            ps.executeUpdate();
+	          } catch (SQLException e) {
+	        	  
+	            e.printStackTrace();
+	            System.out.println("failed to update orderitems");
+	        }
 
 	}
 
 	@Override
 	public void deleteOrderItem(int orderItemId) {
-		// TODO Auto-generated method stub
+		
+		 String query = "delete from order_item where order_item_id=?";
+	        try {
+	        	PreparedStatement ps = con.prepareStatement(query);
+	            ps.setInt(1, orderItemId);
+	            ps.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            System.out.println("failed to delete ordreitem data");
+	        }
 
 	}
 
 	@Override
 	public Orderitems getOrderItemById(int orderItemId) {
-		// TODO Auto-generated method stub
-		return null;
+		Orderitems o=null;
+		String query="select * from order_item where order_item_id=?";
+        try {
+        	PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, orderItemId);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                o = new Orderitems();
+                o.setOrderItemId(rs.getInt("order_item_id"));
+                o.setOrderId(rs.getInt("order_id"));
+                o.setProductId(rs.getInt("product_id"));
+                o.setOrderQuantity(rs.getInt("order_quantity"));
+                o.setUnitPrice(rs.getDouble("unit_price"));
+                }
+            } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("failed to get data");
+            
+        }
+        return o;
 	}
 
 	@Override
 	public List<Orderitems> getAllOrderItems() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Orderitems> list = new ArrayList<>();
+		Orderitems o=null;
+        String query = "select * from order_item";
+        try {
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+            	o = new Orderitems();
+                o.setOrderItemId(rs.getInt("order_item_id"));
+                o.setOrderId(rs.getInt("order_id"));
+                o.setProductId(rs.getInt("product_id"));
+                o.setOrderQuantity(rs.getInt("order_quantity"));
+                o.setUnitPrice(rs.getDouble("unit_price"));
+                list.add(o);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("failed to get list");
+        }
+
+        return list;
 	}
 
 }
